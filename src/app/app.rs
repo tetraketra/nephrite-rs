f02cab37930ce9c75119356fe8a98ae0f94241e7
Tetra Silverberg <tetra.silv@gmail.com>
2026-04-05 13:17:19 -0400

    added: window creation

---
+++ src/app/app.rs
@@ -0,0 +1,63 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::ActiveEventLoop,
+    window::{Window, WindowId},
+};
+
+use crate::vulkan::Context;
+
+#[allow(unused)]
+#[derive(Debug)]
+enum AppState {
+    Uninitialized,
+    Initialized { window: Window, context: Context },
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct App {
+    state: AppState,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            state: AppState::Uninitialized,
+        }
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+    ) {
+        let window = event_loop
+            .create_window(Window::default_attributes().with_title("Nephrite Project"))
+            .expect("Failed to create window.");
+
+        let context = unsafe { Context::create(&window).expect("Failed to create Vulkan context.") };
+
+        self.state = AppState::Initialized { window, context };
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _id: WindowId,
+        event: WindowEvent,
+    ) {
+        match &self.state {
+            AppState::Initialized { window, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    log::info!("Window was closed via close button; exiting.");
+                    event_loop.exit();
+                }
+                WindowEvent::RedrawRequested => window.request_redraw(),
+                _ => (),
+            },
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use winit::event_loop::{ControlFlow, EventLoop};
+
+mod app;
+mod vulkan;
+
+use app::App;
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    let event_loop = EventLoop::new()?;
+    let mut app = App::default();
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.run_app(&mut app).map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(())
 }
